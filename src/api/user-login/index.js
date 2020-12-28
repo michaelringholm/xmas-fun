9401ddfdca0db3555ffcf2c5ed880a9f3fb3417c
Michael Ringholm Sundgaard <michaelringholm@gmail.com>
2020-12-28 19:01:04 +0100

    Tweaked CORS

--- src/api/user-login/index.js
+++ src/api/user-login/index.js
@@ -7,8 +7,10 @@ exports.handler = function(event, context, callback) {
     console.log(JSON.stringify(event));
     //AWS.config.update({region: 'eu-central-1'});
     var method = event.requestContext.http.method;
+    var origin = event.headers.origin;
+    var referer = event.headers.referer;
     if(method == "OPTIONS") {
-        respondOK({}, callback);
+        preFlightResponse(origin, referer, callback);
         return;
     }
     console.log("method="+method);
@@ -25,28 +27,31 @@ exports.handler = function(event, context, callback) {
     };
     
     ddb.getItem(params, function(err, userData) {
-       if (err) { console.log(err); respondError(500, err, callback); }
+       if (err) { console.log(err); respondError(origin, 500, err, callback); }
        else {
             console.log(JSON.stringify(userData));
             if(userData == null || userData.Item == null || userData.Item.password == null) {
                 console.error("User [" + login.userName + "] not found");
-                respondError(401, "Invalid login", callback);
+                respondError(origin, 401, "Invalid login", callback);
             }
             else {
                 if(userData.Item.password.S == login.password) {
                     console.log("Password accepted");
-                    updateToken(login, userData.Item.userGuid.S, ddb, function(tokenData) {
-                        getUserScore(userData.Item.userGuid.S, function(scoreData) {
-                            scoreData.maxTurns = MAX_TURNS;
-                            scoreData.accessToken = tokenData.accessToken;
-                            scoreData.userGuid = tokenData.userGuid;
-                            respondOK(scoreData, callback);
-                        });
+                    updateToken(login, userData.Item.userGuid.S, ddb, function(err, tokenData) {
+                        if (err) { console.log(err); respondError(origin, 500, err, callback); }
+                        else {
+                            getUserScore(userData.Item.userGuid.S, function(scoreData) {
+                                scoreData.maxTurns = MAX_TURNS;
+                                scoreData.accessToken = tokenData.accessToken;
+                                scoreData.userGuid = tokenData.userGuid;
+                                respondOK(origin, scoreData, callback);
+                            });
+                        }
                     });
                 }
                 else {
                     console.error("Wrong password, was [" + login.password + "] exptected [" + userData.Item.password.S + "]");
-                    respondError(401, "Invalid login", callback);
+                    respondError(origin, 401, "Invalid login", callback);
                 }
             }
        }
@@ -67,9 +72,9 @@ function updateToken(login, userGuid, ddb, callback) {
         //ProjectionExpression: 'ATTRIBUTE_NAME'
     };    
     ddb.putItem(params, function(err, userData) {
-        if (err) { console.log(err); respondError(500, err, callback); }        
+        if (err) { console.log(err); callback(err, null); }        
         console.log("New token generated");
-        callback({ "accessToken": newToken, "userGuid": userGuid });
+        callback(null, { "accessToken": newToken, "userGuid": userGuid });
     });    
 }
 
@@ -96,13 +101,16 @@ function getUserScore(userGuid, callback) {
     });
 }
 
-function respondOK(data, callback) {
+function preFlightResponse(origin, referer, callback) {
+    var tweakedOrigin = "";
+    if(origin == "http://localhost" || origin == "http://xmas-fun.s3-website.eu-north-1.amazonaws.com")
+        tweakedOrigin = origin;
+
     const response = {
         statusCode: 200,
-        body: JSON.stringify({ response: 'Login completed', data: data }),
         headers: {
             'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin' : "*", // Required for CORS support to work
+            'Access-Control-Allow-Origin' :   tweakedOrigin,
             'Access-Control-Allow-Credentials' : true, // Required for cookies, authorization headers with HT
             'Access-Control-Allow-Headers' : "content-type"
         },
@@ -110,13 +118,36 @@ function respondOK(data, callback) {
     callback(null, response);
 }
 
-function respondError(errorCode, errorMessage, callback) {
+function respondOK(origin, data, callback) {
+    var tweakedOrigin = "";
+    if(origin == "http://localhost" || origin == "http://xmas-fun.s3-website.eu-north-1.amazonaws.com")
+        tweakedOrigin = origin;
+
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify({ response: 'Login completed', data: data }),
+        headers: {
+            'Content-Type': 'application/json',
+            //'Access-Control-Allow-Origin' : "*", // Required for CORS support to work
+            'Access-Control-Allow-Origin' : tweakedOrigin,
+            'Access-Control-Allow-Credentials' : true, // Required for cookies, authorization headers with HT
+            'Access-Control-Allow-Headers' : "content-type"
+        },
+    };
+    callback(null, response);
+}
+
+function respondError(origin, errorCode, errorMessage, callback) {
+    var tweakedOrigin = "";
+    if(origin == "http://localhost" || origin == "http://xmas-fun.s3-website.eu-north-1.amazonaws.com")
+        tweakedOrigin = origin;
+
     const response = {
         statusCode: errorCode,
         body: JSON.stringify({ response: errorMessage }),
         headers: {
             'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin' : "*", // Required for CORS support to work
+            'Access-Control-Allow-Origin' : tweakedOrigin, // Required for CORS support to work
             'Access-Control-Allow-Credentials' : true, // Required for cookies, authorization headers with HT
             'Access-Control-Allow-Headers' : "content-type"
         },
